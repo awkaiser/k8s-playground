0e7242330bfc3ea0d52b5973a9a4b5ce0c48630c
August "Kai" Kaiser <awkaiser@users.noreply.github.com>
2024-12-21 00:12:00 -0500

    Add `sablier` Traefik plugin

--- Tiltfile
+++ Tiltfile
@@ -1,3 +1,4 @@
 include('./apps/deno-fastify/Tiltfile')
 include('./apps/deno-hono/Tiltfile')
+# include('./apps/sablier/Tiltfile')
 include('./apps/traefik/Tiltfile')
---
+++ scripts/kube-ctl-crd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Traefik Resource Definitions:
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.2/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+
+# Install RBAC for Traefik:
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.2/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
